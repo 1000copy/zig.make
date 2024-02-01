fb3754fdfb3099a0cab3a7590303f3d16a454ff0
1000copy <1000copy@gmail.com>
2024-02-01 10:27:57 +0800

    completed on MAC and Windows

--- build.zig
+++ build.zig
@@ -3,6 +3,13 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     var sources = std.ArrayList([]const u8).init(b.allocator);
     // std.debug.print("{s}", .{a[0]});
+    const bbmac = .{
+        "concat-filename",
+        "findprog-in",
+        "fnmatch",
+        "glob",
+    };
+    const aamac = .{ "ar", "arscan", "commands", "default", "dir", "expand", "file", "function", "getopt", "getopt1", "guile", "hash", "implicit", "job", "load", "loadapi", "main", "misc", "output", "read", "remake", "rule", "shuffle", "signame", "strcache", "variable", "version", "vpath", "posixos", "remote-stub" };
     const aa = .{ "ar", "arscan", "commands", "default", "dir", "expand", "file", "function", "getopt", "getopt1", "guile", "hash", "implicit", "job", "load", "loadapi", "main", "misc", "output", "read", "remake", "remote-stub", "rule", "shuffle", "signame", "strcache", "variable", "version", "vpath" };
     const cc = .{ "pathstuff", "w32os", "compat/posixfcn", "subproc/misc", "subproc/sub_proc", "subproc/w32err" };
     const bb = .{ "fnmatch", "glob", "getloadavg" };
@@ -20,26 +27,45 @@ pub fn build(b: *std.Build) !void {
     // WINDOWS32
     exe.addIncludePath(std.build.LazyPath.relative("."));
     exe.addIncludePath(std.build.LazyPath.relative("./src/"));
-    exe.addIncludePath(std.build.LazyPath.relative("./src/w32/include/"));
+
     exe.addIncludePath(std.build.LazyPath.relative("./lib/"));
 
     // exe.defineCMacro("__MINGW32__", null);
-    exe.defineCMacro("_CONSOLE", null);
     exe.defineCMacro("HAVE_CONFIG_H", null);
-    exe.defineCMacro("WINDOWS32", null);
-    exe.defineCMacro("STDC_HEADERS", null);
-    inline for (aa) |c| {
-        try sources.append("src/" ++ c ++ ".c");
+    if (exe.target.isWindows()) {
+        exe.addIncludePath(std.build.LazyPath.relative("./src/w32/include/"));
+
+        exe.defineCMacro("_CONSOLE", null);
+        exe.defineCMacro("WINDOWS32", null);
     }
-    inline for (bb) |c| {
-        try sources.append("lib/" ++ c ++ ".c");
+    if (exe.target.isDarwin()) {
+        exe.defineCMacro("STDC_HEADERS", null);
+        exe.defineCMacro("__STDC__", null);
+        // exe.defineCMacro("ENABLE_NLS", null);
+        exe.defineCMacro("LIBDIR", "\".\"");
+        exe.defineCMacro("LOCALEDIR", "\"/usr/local/share/locale\"");
+        exe.defineCMacro("__GNU_LIBRARY__", null);
+    }
+    if (exe.target.isDarwin()) {
+        inline for (aamac) |c| {
+            try sources.append("src/" ++ c ++ ".c");
+        }
+        inline for (bbmac) |c| {
+            try sources.append("lib/" ++ c ++ ".c");
+        }
     }
     if (exe.target.isWindows()) {
         inline for (cc) |c| {
             try sources.append("src/w32/" ++ c ++ ".c");
         }
+        inline for (aa) |c| {
+            try sources.append("src/" ++ c ++ ".c");
+        }
+        inline for (bb) |c| {
+            try sources.append("lib/" ++ c ++ ".c");
+        }
     }
-    exe.addCSourceFiles(sources.items, &.{});
+    exe.addCSourceFiles(sources.items, &.{"-Wstring-compare"});
     exe.linkLibC();
     //exe.addIncludePath(std.build.LazyPath.relative("bass/linux"));
     b.installArtifact(exe);
